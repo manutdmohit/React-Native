b9c893fa100881c42033d4e016a1cf1698a8c814
manutdmohit <saudmohit@gmail.com>
2021-11-18 20:12:36 +0545

    Understaning Lists using FlatList

---
+++ src/screens/ListsScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+const ListsScreen = () => {
+  const friends = [
+    { name: 'A' },
+    { name: 'B' },
+    { name: 'C' },
+    { name: 'D' },
+    { name: 'E' },
+  ];
+
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      renderItem={({ item }) => (
+        <Text style={styles.textStyle}>{item.name}</Text>
+      )}
+    />
+  );
+};
+
+export default ListsScreen;
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    marginVertical: 50,
+  },
+});
