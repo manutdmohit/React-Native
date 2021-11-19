40773d79b2f6cd037ebfe6b42941c4352f651e75
manutdmohit <saudmohit@gmail.com>
2021-11-19 09:52:49 +0545

    destructing props to navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
         title="Go to Components Demo"
+        onPress={() => navigation.navigate('Components')}
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button title="Go to List" onPress={() => navigation.navigate('List')} />
     </View>
   );
 };
