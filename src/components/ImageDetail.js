1ffc2eb7dd99a427e5ac0d5f0bf7955f43e91b98
manutdmohit <saudmohit@gmail.com>
2021-11-20 10:48:44 +0545

    passing images as props

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const ImageDetail = (props) => {
   return (
     <View>
+      <Image source={props.imageSource} style={{ height: 100, width: 100 }} />
       <Text> {props.title}</Text>
     </View>
   );
 };
 
-export default ImageDetail;
 const styles = StyleSheet.create({});
+
+export default ImageDetail;
