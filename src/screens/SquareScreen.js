63c8b53f6cfcce65668502c47a09f3d5260fddeb
manutdmohit <saudmohit@gmail.com>
2021-11-21 10:08:20 +0545

    Tying state values together

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const CHANGE_COLOR_VALUE = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
@@ -9,22 +11,35 @@ const SquareScreen = () => {
 
   console.log(red, green, blue);
 
+  if ((red || green || blue) > 255 || (red || green || blue) < 0) {
+  }
+
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + CHANGE_COLOR_VALUE)}
+        onDecrease={() => setRed(red - CHANGE_COLOR_VALUE)}
         color="Red"
       />
+
       <ColorCounter
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => setGreen(green + CHANGE_COLOR_VALUE)}
+        onDecrease={() => setGreen(green - CHANGE_COLOR_VALUE)}
+        color="Green"
+      />
+
+      <ColorCounter
+        onIncrease={() => setBlue(blue + CHANGE_COLOR_VALUE)}
+        onDecrease={() => setBlue(blue - CHANGE_COLOR_VALUE)}
         color="Blue"
       />
-      <ColorCounter
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
-        color="Green"
+
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${green}, ${blue})`,
+        }}
       />
     </View>
   );
