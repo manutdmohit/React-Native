c55cf3baeb49f44282db707fdcb74c76a3693d5b
manutdmohit <saudmohit@gmail.com>
2021-11-24 15:26:50 +0545

    align items with flex

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { Text, View, StyleSheet } from 'react-native';
 export const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
@@ -13,11 +15,12 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
+
+    alignItems: 'flex-end',
   },
   textStyle: {
-    borderWidth: 10,
+    borderWidth: 3,
     borderColor: 'red',
-    margin: 20,
   },
 });
 
